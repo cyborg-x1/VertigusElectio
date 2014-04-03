2792f25b614a7bc53611362ceac3a715674f88dd
Christian Holl <choll@synapticon.com>
2014-04-04 01:50:34 +0200

    define LCDBaseMenuEntry and LCDMenu

--- include/lcdmenu.hpp
+++ include/lcdmenu.hpp
@@ -12,11 +12,62 @@
 
 namespace VertigusElectio {
 
+class LCDBaseMenuEntry
+{
+
+	const char *entryname;
+	bool isDisabled;
+	LCDBaseMenuEntry *next;
+	LCDBaseMenuEntry *prev;
+
+public:
+	LCDBaseMenuEntry(const char * entryname)
+	:entryname(entryname)
+	{
+		this->next=0;
+		this->prev=0;
+		this->isDisabled=false;
+	}
+	virtual ~LCDBaseMenuEntry(){};
+
+
+	void setDisabled(bool disabled);
+
+	virtual void on_up()=0;
+	virtual void on_down()=0;
+	virtual void on_button()=0;
+};
+
+
 class LCDMenu {
 	BaseLCDisplay *lcd;
+	LCDBaseMenuEntry *current;
+
 public:
-	LCDMenu(BaseLCDisplay *lcd);
+
+	typedef enum
+	{
+		ALPHABETIC_UP,
+		ALPHABETIC_DOWN,
+		INSERTION,
+	}SortRule;
+
+	typedef enum
+	{
+		HIDE,
+		DISABLE_CHAR,
+		DISPLAY_ALT,
+	}DisableRule;
+
+
+	LCDMenu(BaseLCDisplay *lcd, SortRule sortrule, DisableRule disablerule, char disabledChar='#');
 	virtual ~LCDMenu();
+
+	void addEntry(LCDBaseMenuEntry* entry);
+
+	void up();
+	void down();
+	void button();
 };
 
 } /* namespace VertigusElectio */
